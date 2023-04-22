4f3255926ac4d461ea016bf81c1445d5c64bed73
NamesMark <markfirn@gmail.com>
2023-04-22 22:41:01 +0300

    Tried chrome-webstore-upload-cli

--- content.js
+++ content.js
@@ -12,144 +12,144 @@ function hideButtons() {
 
 function comicSansThem(tweetContainer, apply) {
     const isVerified = tweetContainer.querySelector('svg[aria-label="Verified account"]');
-  
+
     if (isVerified) {
-      const tweetText = tweetContainer.querySelector('div[lang]');
-      const username = tweetContainer.querySelector('div[dir="ltr"] > span');
-  
-      if (apply) {
+        const tweetText = tweetContainer.querySelector('div[lang]');
+        const username = tweetContainer.querySelector('div[dir="ltr"] > span');
+
+        if (apply) {
         if (tweetText) {
-          tweetText.style.fontFamily = 'Comic Sans MS, sans-serif';
-          tweetText.style.fontSize = '12px';
-          tweetText.style.fontStyle = 'italic';
+            tweetText.style.fontFamily = 'Comic Sans MS, sans-serif';
+            tweetText.style.fontSize = '12px';
+            tweetText.style.fontStyle = 'italic';
         }
         if (username) {
-          username.style.fontFamily = 'Comic Sans MS, sans-serif';
+            username.style.fontFamily = 'Comic Sans MS, sans-serif';
         }
-      } else {
+        } else {
         if (tweetText) {
-          tweetText.style.fontFamily = '';
-          tweetText.style.fontSize = '';
-          tweetText.style.fontStyle = '';
+            tweetText.style.fontFamily = '';
+            tweetText.style.fontSize = '';
+            tweetText.style.fontStyle = '';
         }
         if (username) {
-          username.style.fontFamily = '';
+            username.style.fontFamily = '';
         }
-      }
+        }
     }
-  }
-  
-  // Refactored applyChanges function
-  function applyChanges() {
-    hideButtons();
+}
+
+function applyComicSans() {
+    const tweetContainers = document.querySelectorAll('article[role="article"]');
+    tweetContainers.forEach(tweetContainer => {
+        comicSansThem(tweetContainer, true);
+    });
+}
+
+function removeComicSans() {
+    const tweetContainers = document.querySelectorAll('article[role="article"]');
+    tweetContainers.forEach(tweetContainer => {
+        comicSansThem(tweetContainer, false);
+    });
+}
+
+function hideBlueTweets() {
+    const tweetContainers = document.querySelectorAll('article[role="article"]');
+    tweetContainers.forEach(tweetContainer => {
+        const isVerified = tweetContainer.querySelector('svg[aria-label="Verified account"]');
+        if (isVerified) {
+        tweetContainer.style.display = 'none';
+        }
+    });
+}
+
+function showBlueTweets() {
+    const tweetContainers = document.querySelectorAll('article[role="article"]');
+    tweetContainers.forEach(tweetContainer => {
+        const isVerified = tweetContainer.querySelector('svg[aria-label="Verified account"]');
+        if (isVerified) {
+        tweetContainer.style.display = '';
+        }
+    });
+}
+
+function applyChanges() {
+    if (hideBlueButtonsState) {
+        hideButtons();
+    }
     const hideBlueTweetsState = localStorage.getItem('hideBlueTweets') === 'true';
     const comicSansBlueTweetsState = localStorage.getItem('comicSansBlueTweets') === 'true';
-  
+
     const tweetContainers = document.querySelectorAll('article[role="article"]');
     tweetContainers.forEach(tweetContainer => {
-      comicSansThem(tweetContainer, comicSansBlueTweetsState);
-      if (hideBlueTweetsState) {
+        comicSansThem(tweetContainer, comicSansBlueTweetsState);
+        if (hideBlueTweetsState) {
         hideBlueTweets();
-      } else {
+        } else {
         showBlueTweets();
-      }
+        }
     });
-  
+
     const targetNode = document.querySelector('body');
     const config = { childList: true, subtree: true };
-  
+
     const observer = new MutationObserver((mutationsList, observer) => {
-      for (const mutation of mutationsList) {
+        for (const mutation of mutationsList) {
         if (mutation.type === 'childList') {
-          mutation.addedNodes.forEach((node) => {
+            mutation.addedNodes.forEach((node) => {
             if (node.nodeType === Node.ELEMENT_NODE) {
-              const newTweets = node.querySelectorAll('article[role="article"]');
-              newTweets.forEach(tweetContainer => {
+                const newTweets = node.querySelectorAll('article[role="article"]');
+                newTweets.forEach(tweetContainer => {
                 comicSansThem(tweetContainer, comicSansBlueTweetsState);
                 if (hideBlueTweetsState) {
-                  hideBlueTweets();
+                    hideBlueTweets();
                 } else {
-                  showBlueTweets();
+                    showBlueTweets();
                 }
-              });
+                });
             }
-          });
+            });
         }
-      }
+        }
     });
-  
+
     observer.observe(targetNode, config);
-  }
-  
-  // Update the message listener to store the menu items state in localStorage
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     switch (request.action) {
-      case 'toggleHideBlueButtons':
+        case 'toggleHideBlueButtons':
         localStorage.setItem('hideBlueButtons', request.value);
         if (request.value) {
-          hideButtons();
+            hideButtons();
         } else {
-          showButtons();
+            showButtons();
         }
         break;
-      case 'toggleComicSansBlueTweets':
+        case 'toggleComicSansBlueTweets':
         localStorage.setItem('comicSansBlueTweets', request.value);
         if (request.value) {
-          applyComicSans();
+            applyComicSans();
         } else {
-          removeComicSans();
+            removeComicSans();
         }
         break;
-      case 'toggleHideBlueTweets':
+        case 'toggleHideBlueTweets':
         localStorage.setItem('hideBlueTweets', request.value);
         if (request.value) {
-          hideBlueTweets();
+            hideBlueTweets();
         } else {
-          showBlueTweets();
+            showBlueTweets();
         }
         break;
     }
-  });
+});
 
-  function applyComicSans() {
-    const tweetContainers = document.querySelectorAll('article[role="article"]');
-    tweetContainers.forEach(tweetContainer => {
-      comicSansThem(tweetContainer, true);
-    });
-  }
-  
-  function removeComicSans() {
-    const tweetContainers = document.querySelectorAll('article[role="article"]');
-    tweetContainers.forEach(tweetContainer => {
-      comicSansThem(tweetContainer, false);
-    });
-  }
-
-  function hideBlueTweets() {
-    const tweetContainers = document.querySelectorAll('article[role="article"]');
-    tweetContainers.forEach(tweetContainer => {
-      const isVerified = tweetContainer.querySelector('svg[aria-label="Verified account"]');
-      if (isVerified) {
-        tweetContainer.style.display = 'none';
-      }
-    });
-  }
-  
-  function showBlueTweets() {
-    const tweetContainers = document.querySelectorAll('article[role="article"]');
-    tweetContainers.forEach(tweetContainer => {
-      const isVerified = tweetContainer.querySelector('svg[aria-label="Verified account"]');
-      if (isVerified) {
-        tweetContainer.style.display = '';
-      }
-    });
-  }
-  
-  window.onload = () => {
+window.onload = () => {
     const hideBlueButtonsState = localStorage.getItem('hideBlueButtons') === 'true';
     if (hideBlueButtonsState) {
-      setTimeout(hideButtons, 1000);
-      setTimeout(hideButtons, 2000);
+        setTimeout(hideButtons, 1000);
+        setTimeout(hideButtons, 2000);
     }
     setTimeout(applyChanges, 3000);
-  };
+};
